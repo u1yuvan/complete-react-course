74287c3c5da9098b08bf598378654bc37ea5611f
u1yuvan <yuvanraj700@gmail.com>
2023-01-04 20:43:45 +0530

    First_React_Project

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const header = <h1>This is my first React App</h1>;
+
+// console.log(header);
+
+ReactDOM.render(header,document.getElementById('root'));
+
