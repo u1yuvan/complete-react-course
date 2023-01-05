1daef8027224ce592238777c6539f791c525c012
u1yuvan <yuvanraj700@gmail.com>
2023-01-05 19:32:18 +0530

    Adding CSS to Component

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
+import './app.css'
+
 function App() {
     let msg = "awesome";
 
     return (<div>
-        <h1>This is my Header</h1>
-            <p>This is my first {msg}component </p>
+        <h1 className="header">This is my Header</h1>
+        <p className="para">This is my first {msg} component </p>
     </div>);
 }
 
