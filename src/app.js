63e0679af2122adff69564ec62628b20fc6e0c7f
u1yuvan <yuvanraj700@gmail.com>
2023-01-05 19:13:10 +0530

    JavaScript XML

---
+++ src/app.js
@@ -0,0 +1,11 @@
+function App() {
+    let msg = "awesome";
+
+    return (<div>
+        <h1>This is my Header</h1>
+            <p>This is my first {msg}component </p>
+    </div>);
+}
+
+
+export default App;
