5eb59a90d2fd8d42feb166ae816310c42cb8b4f8
u1yuvan <yuvanraj700@gmail.com>
2023-01-17 14:06:36 +0530

    A closer look at JSX Syntax

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+function Button(props){
+    return  <button className="btn btn-primary">{props.children}</button>
+
+}
+
+export default Button;
---
+++ src/components/ProductDetails.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Button from "./Button";
+
+let productCount = 1;
+function displayFormattedProductCount() {
+    return productCount > 0 ? productCount : 'Zero'
+}
+
+
+function ProductDetails(props) {
+    let badgeClass = 'badge-margin-left-240  badge ';
+    badgeClass += props.isAvailable ? 'bg-success' : 'bg-danger';
+
+    return (
+        <div className="d-flex align-items-center justify-content-start mt-1" >
+            <h6 className="font-weight-bold my-2" style={{ 'marginRight': 30 }}>{props.price}₹</h6>
+            <Button >-</Button>
+            <span style={{ 'padding': '0px 20px', "fontSize": 14, }}>
+                {displayFormattedProductCount()}
+            </span>
+            <Button>+</Button>
+            <span className={badgeClass}>{props.isAvailable ? 'Avilable' : 'Unavailable'}</span>
+        </div>
+    )
+    // return React.createElement('div', {className: 'd-flex align-items-center justify-content-start mt-1'}, 
+    //           React.createElement('h6',{className:"font-weight-bold my-2", style: {marginRight: 30 }}, "₹" + props.price ),
+    //           React.createElement(Button, {}, "-"),
+    //           React.createElement('span', {style: {padding: '0px 20px', fontSize: 14}},displayFormattedProductCount()),
+    //           React.createElement(Button, {}, "+"),
+    //           React.createElement('span', {className: badgeClass}, props.isAvailable ? "Available" : "UnAvailable")
+    //           )
+}
+
+export default ProductDetails;
