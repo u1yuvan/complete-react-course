39dfd87a5c95732f08d09eb884026c5df4f87cec
u1yuvan <yuvanraj700@gmail.com>
2023-01-19 12:17:22 +0530

    Working with State in React

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Button from "./Button";
 
 
@@ -8,32 +8,35 @@ function ProductDetails(props) {
     let badgeClass = 'badge-margin-left-240  badge ';
     badgeClass += props.isAvailable ? 'bg-success' : 'bg-danger';
 
-    let productCount = 0;
+   let [productCount,updateCount] = useState(0)
+    // let productCount = 0;
     function displayFormattedProductCount() {
         return productCount > 0 ? productCount : 'Zero'
     }
 
     let incrementProductCount = function () {
-        productCount++;
-        console.log(productCount);
+        // productCount++;
+        // console.log(productCount);
+        updateCount(++productCount)
     }
 
     let decrementProductCount = function () {
-        productCount--;
-        console.log(productCount);
+        // productCount--;
+        // console.log(productCount);
+        updateCount(--productCount)
     }
 
-    // return (
-    //     <div className="d-flex align-items-center justify-content-start mt-1" >
-    //         <h6 className="font-weight-bold my-2" style={{ 'marginRight': 30 }}>{props.price}₹</h6>
-    //         <Button eventHandler={decrementProductCount} >-</Button>
-    //         <span style={{ 'padding': '0px 20px', "fontSize": 14, }}>
-    //             {displayFormattedProductCount()}
-    //         </span>
-    //         <Button eventHandler={incrementProductCount}>+</Button>
-    //         <span className={badgeClass}>{props.isAvailable ? 'Avilable' : 'Unavailable'}</span>
-    //     </div>
-    // )
+    return (
+        <div className="d-flex align-items-center justify-content-start mt-1" >
+            <h6 className="font-weight-bold my-2" style={{ 'marginRight': 30 }}>{props.price}₹</h6>
+            <Button eventHandler={decrementProductCount} >-</Button>
+            <span style={{ 'padding': '0px 20px', "fontSize": 14, }}>
+                {displayFormattedProductCount()}
+            </span>
+            <Button eventHandler={incrementProductCount}>+</Button>
+            <span className={badgeClass}>{props.isAvailable ? 'Avilable' : 'Unavailable'}</span>
+        </div>
+    )
     // return React.createElement('div', {className: 'd-flex align-items-center justify-content-start mt-1'}, 
     //           React.createElement('h6',{className:"font-weight-bold my-2", style: {marginRight: 30 }}, "₹" + props.price ),
     //           React.createElement(Button, {}, "-"),
