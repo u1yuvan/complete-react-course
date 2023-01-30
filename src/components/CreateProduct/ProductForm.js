a80fab214e85e3d74762f91b579044e91b47aabd
u1yuvan <yuvanraj700@gmail.com>
2023-01-30 14:04:49 +0530

    Working with multiple States

--- src/components/CreateProduct/ProductForm.js
+++ src/components/CreateProduct/ProductForm.js
@@ -1,24 +1,32 @@
+import React,{ useState } from "react";
+
 function ProductForm() {
+    let[pName, updateName]= useState('')
+    let[pPrice, updatePrice]= useState('')
+    let[pDescription, updateDescription]= useState('')
+    let[pAvailable, updateAvailability]= useState('')
+    let[pImageUrl, updateImageUrl]= useState('')
+    
    
 
     function nameInputHandler(event) {
-        console.log(event.target.value);
+        updateName(event.target.value);
     }
 
     function priceInputHandler(event) {
-        console.log(event.target.value);
+        updatePrice(event.target.value);
     }
     
     function descriptionInputHandler(event) {
-        console.log(event.target.value);
+        updateDescription(event.target.value);
     }
 
     function availableInputHandler(event) {
-        console.log(event.target.value);
+        updateAvailability(event.target.value);
     }
 
     function imageInputHandler(event) {
-        console.log(event.target.value);
+        updateImageUrl(event.target.value);
     }
 
     return (
